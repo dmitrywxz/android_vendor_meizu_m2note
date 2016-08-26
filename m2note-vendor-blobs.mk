969bdc813a24d64056ef18e3f4b020711df2dbfa
Moyster <oysterized@gmail.com>
2016-08-26 22:49:11 +0200

    remove boot logo updater binary

--- m2note-vendor-blobs.mk
+++ m2note-vendor-blobs.mk
@@ -40,7 +40,6 @@ PRODUCT_COPY_FILES += \
     vendor/meizu/m2note/proprietary/system/bin/autokd:system/bin/autokd \
     vendor/meizu/m2note/proprietary/system/bin/badblocks:system/bin/badblocks \
     vendor/meizu/m2note/proprietary/system/bin/batterywarning:system/bin/batterywarning \
-    vendor/meizu/m2note/proprietary/system/bin/boot_logo_updater:system/bin/boot_logo_updater \
     vendor/meizu/m2note/proprietary/system/bin/factory:system/bin/factory \
     vendor/meizu/m2note/proprietary/system/bin/lcdc_screen_cap:system/bin/lcdc_screen_cap \
     vendor/meizu/m2note/proprietary/system/bin/bmm050d:system/bin/bmm050d \
